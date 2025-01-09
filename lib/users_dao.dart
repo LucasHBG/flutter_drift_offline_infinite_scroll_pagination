b235a59ae8be396fde63e26541a6d94ab8dd44de
Lucas H. B. Guimaraes <lucas.guimaraes@irancho.com.br>
2025-01-09 17:48:51 -0300

    feat: added getUsers function

--- lib/users_dao.dart
+++ lib/users_dao.dart
@@ -8,6 +8,10 @@ part 'users_dao.g.dart';
 class UsersDao extends DatabaseAccessor<AppDatabase> with _$UsersDaoMixin {
   UsersDao(super.db);
 
+  Future<List<User>> getUsers(int offset) async {
+    return await select(users).get();
+  }
+
   // A auto-generated User class will be create in order to be used as a type and at the view page
   Future<List<User>> getUsersPaginated(int offset) async {
     return await (select(users)..limit(10, offset: offset)).get();
